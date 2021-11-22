4c7e2f71e513b7b20fbe04032599f907c959daeb
chunu-j <codehouse9@naver.com>
2021-11-22 15:08:42 +0900

    Modify: local.strategy 의 필드에 맞는값을 넣어주어 인증 오류 해결

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,18 +1,20 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-local";
-import { AuthService } from "../auth.service";
-
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+import { Strategy } from 'passport-local';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly authService: AuthService) {
-        super();
-    }
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'id', // 필드에 맞게 설정해줘야 한다.
+      passwordField: 'password',
+    });
+  }
 
-    async validate(username: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(username, password);
-        if (!user) throw new UnauthorizedException();
-        return user;
-    }
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
 }
